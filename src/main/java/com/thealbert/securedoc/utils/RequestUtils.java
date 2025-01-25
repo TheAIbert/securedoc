d7532aa884f861d11dc9a54d5dbc3bb0bd235089
Albert <thealberthyan@gamil.com>
2025-01-25 22:15:33 +0400

    Implement CommandLineRunner for role creation and add System user (ID 0)

--- src/main/java/com/thealbert/securedoc/utils/RequestUtils.java
+++ src/main/java/com/thealbert/securedoc/utils/RequestUtils.java
@@ -7,13 +7,14 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import static java.time.LocalDateTime.now;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class RequestUtils {
 
     public static Response getResponse(HttpServletRequest request, Map<?, ?> data, String message, HttpStatus status) {
         return new Response(
-                LocalDateTime.now().toString(),
+                now().toString(),
                 status.value(),
                 request.getRequestURI(),
                 HttpStatus.valueOf(status.value()),
