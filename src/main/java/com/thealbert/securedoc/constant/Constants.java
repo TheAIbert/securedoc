e1091d4773254d201fd575728e3fe897d957be68
Albert <thealberthyan@gamil.com>
2025-04-11 19:23:45 +0400

    - User Mapping - User Principal - ApiAuthenticationProvider - Constants

--- src/main/java/com/thealbert/securedoc/constant/Constants.java
+++ src/main/java/com/thealbert/securedoc/constant/Constants.java
@@ -1,7 +1,13 @@
 package com.thealbert.securedoc.constant;
 
 public class Constants {
-
+    public static final int STRENGTH = 12;
+    public static final int NINETY_DAYS = 90;
+    public static final String LOGIN_PATH = "/user/login";
+    public static final String AUTHORITIES = "authorities";
+    public static final String THE_ALBERT_LLC = "THE_ALBERT_LLC";
+    public static final String EMPTY_VALUE = "empty";
+    public static final String ROLE = "role";
     public static final String ROLE_PREFIX = "ROLE_";
     public static final String AUTHORITY_DELIMITER = ",";
     public static final String USER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
--- src/main/java/com/thealbert/securedoc/utils/UserUtils.java
+++ src/main/java/com/thealbert/securedoc/utils/UserUtils.java
@@ -1,10 +1,14 @@
 package com.thealbert.securedoc.utils;
 
+import com.thealbert.securedoc.dto.User;
+import com.thealbert.securedoc.entity.CredentialEntity;
 import com.thealbert.securedoc.entity.RoleEntity;
 import com.thealbert.securedoc.entity.UserEntity;
+import org.springframework.beans.BeanUtils;
 
 import java.util.UUID;
 
+import static com.thealbert.securedoc.constant.Constants.NINETY_DAYS;
 import static java.time.LocalDateTime.now;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
@@ -29,5 +33,22 @@ public class UserUtils {
                 .imageUrl("https://cdn-icons-png.flaticon.com/512/149/149071.png")
                 .role(role)
                 .build();
+    }
+
+
+    public static User fromUserEntity(UserEntity userEntity, RoleEntity role, CredentialEntity credentialEntity) {
+        User user = new User();
+        BeanUtils.copyProperties(userEntity, user);
+        user.setLastLogin(userEntity.getLastLogin().toString());
+        user.setCredentialsNonExpired(isCredentialsNonExpired(credentialEntity));
+        user.setCreatedAt(userEntity.getCreatedAt().toString());
+        user.setUpdatedAt(userEntity.getUpdatedAt().toString());
+        user.setRole(role.getName());
+        user.setAuthorities(role.getAuthorities().getValue());
+        return user;
+    }
+
+    public static boolean isCredentialsNonExpired(CredentialEntity credentialEntity) {
+        return credentialEntity.getUpdatedAt().plusDays(NINETY_DAYS).isAfter(now());
     }
 }
